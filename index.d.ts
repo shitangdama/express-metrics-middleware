b0c64634581bbf40177cd0fcb787ac7adb5d4d15
i536596 <>
2025-07-10 13:24:28 +0800

    add_export_client

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,5 @@ declare module "express-metrics-middleware" {
     export function signalIsNotUp(): void;
     export function signalIsUp(): void;
     export function createMiddleware(option:Object);
+    export const client;
 }
